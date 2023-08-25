77c76e18c5c6a0bdca20066cd197fdd9ab8674d9
Xilites <3451835817@qq.com>
2023-08-25 14:27:17 +0800

    v0.0.3.0013

--- src/example/content/ModBlocks.java
+++ src/example/content/ModBlocks.java
@@ -1,10 +1,16 @@
 package example.content;
 
+import mindustry.content.Items;
+import mindustry.type.Category;
+import mindustry.world.Block;
 import mindustry.world.blocks.campaign.LaunchPad;
 import mindustry.world.blocks.storage.CoreBlock;
+import mindustry.world.meta.BuildVisibility;
+
+import static mindustry.type.ItemStack.with;
 
 public class ModBlocks {
-    public static CoreBlock Old_World_Core;
+    public static Block Old_World_Core;
     public static LaunchPad Old_World_Launch_Pad;
     public static void load(){
         Old_World_Launch_Pad = new LaunchPad("OldWorldLaunchPad"){
@@ -14,6 +20,8 @@ public class ModBlocks {
         };
         Old_World_Core = new CoreBlock("OldWorldCore"){
             {
+                requirements(Category.effect, BuildVisibility.editorOnly, with(Items.beryllium, 5));
+
                 alwaysUnlocked = true;
                 isFirstTier = true;
                 health = 10000;
