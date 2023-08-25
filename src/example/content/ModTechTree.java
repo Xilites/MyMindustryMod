d25edb6cd62eab15b86efb96042f793b55056ef0
Xilites <3451835817@qq.com>
2023-08-25 14:01:58 +0800

    v0.0.3.0011

--- src/example/content/ModTechTree.java
+++ src/example/content/ModTechTree.java
@@ -1,13 +1,18 @@
 package example.content;
 
+import arc.struct.Seq;
 import mindustry.content.Planets;
+import mindustry.game.Objectives;
 
+import static mindustry.content.TechTree.node;
 import static mindustry.content.TechTree.nodeRoot;
 
 public class ModTechTree {
     public static void load(){
         ModPlanet.OldWorld.techTree = nodeRoot("OldWorld",ModBlocks.Old_World_Launch_Pad,() -> {
+            node(ModBlocks.Old_World_Launch_Pad,() -> {
 
+            });
         });
     }
 }
