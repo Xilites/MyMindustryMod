bd6e8e504198133d035bae16343d62786a9d0f94
Xilites <3451835817@qq.com>
2023-08-25 14:18:50 +0800

    v0.0.3.0012

--- src/example/content/ModBlocks.java
+++ src/example/content/ModBlocks.java
@@ -1,13 +1,24 @@
 package example.content;
 
 import mindustry.world.blocks.campaign.LaunchPad;
+import mindustry.world.blocks.storage.CoreBlock;
 
 public class ModBlocks {
+    public static CoreBlock Old_World_Core;
     public static LaunchPad Old_World_Launch_Pad;
     public static void load(){
         Old_World_Launch_Pad = new LaunchPad("OldWorldLaunchPad"){
             {
 
+            }
+        };
+        Old_World_Core = new CoreBlock("OldWorldCore"){
+            {
+                alwaysUnlocked = true;
+                isFirstTier = true;
+                health = 10000;
+                itemCapacity = 40000;
+                size = 2;
             }
         };
     };
--- src/example/content/ModTechTree.java
+++ src/example/content/ModTechTree.java
@@ -4,12 +4,11 @@ import arc.struct.Seq;
 import mindustry.content.Planets;
 import mindustry.game.Objectives;
 
-import static mindustry.content.TechTree.node;
-import static mindustry.content.TechTree.nodeRoot;
+import static mindustry.content.TechTree.*;
 
 public class ModTechTree {
     public static void load(){
-        ModPlanet.OldWorld.techTree = nodeRoot("OldWorld",ModBlocks.Old_World_Launch_Pad,() -> {
+        ModPlanet.OldWorld.techTree = nodeRoot("OldWorld",ModBlocks.Old_World_Core,() -> {
             node(ModBlocks.Old_World_Launch_Pad,() -> {
 
             });
